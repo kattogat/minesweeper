873172ff60078a275720c6670fea822795e18dee
kattogat <maria_hav@live.se>
2020-07-24 14:09:04 +0200

    Show mines and numbers

--- game.js
+++ game.js
@@ -35,7 +35,7 @@ function createFeild(x, y, mine) {
         let new_square = document.createElement("div");
         new_square.classList.add("square");
         new_square.id = count;
-        new_square.addEventListener("click", function(){ check(this); })
+        new_square.addEventListener("click", function(){ check(this); });
         feild.appendChild(new_square);
 
         count++;
@@ -60,7 +60,6 @@ function check (element) {
             let placment = Math.floor(Math.random() * (square_amount - 1 + 1)) + 1;
             // First square can't be a mine
             while (placment == element.id) {
-                console.log(placment);
                 placment = Math.floor(Math.random() * (square_amount - 1 + 1)) + 1;
             }
 
@@ -69,7 +68,56 @@ function check (element) {
             }
         }
     }
-    
+
     element.classList.remove("square");
     element.classList.add("blank-square");
+
+    // Did they hit a mine?
+    if (mines[parseInt(element.id)] === true) {
+        element.classList.add("mine");
+        return;
+    }
+
+    // Check suronding squares
+    const surrounding_mines = checkSurondingSquares(element.id);
+    if (surrounding_mines > 0) {
+        // They hit a number
+        element.innerHTML = surrounding_mines;
+        return;
+    }
+}
+
+/**
+ * See what squares tuching the id has
+ * @param {*} id 
+ */
+function checkSurondingSquares(id) {
+    let surrounding = closeBy(id);
+    let surrounding_mines = 0;
+
+    for (let i = 0; i < surrounding.length; i++) {
+        if (mines[surrounding[i]] === true) surrounding_mines++;
+    }
+
+    return surrounding_mines;
+}
+
+/**
+ * Get all close by id's 
+ * @param {*} original_id 
+ */
+function closeBy(original_id) {
+    original_id = parseInt(original_id);
+    let surrounding = [];
+
+    if (document.getElementById(original_id + 1) !== null) surrounding.push(parseInt(document.getElementById(original_id + 1).id)); // forward
+    if (document.getElementById(original_id - 1) !== null) surrounding.push(parseInt(document.getElementById(original_id - 1).id)); // backward
+    if (document.getElementById(original_id + y_lenght) !== null) surrounding.push(parseInt(document.getElementById(original_id + y_lenght).id)); // top
+    if (document.getElementById((original_id + y_lenght) + 1) !== null) surrounding.push(parseInt(document.getElementById((original_id + y_lenght) + 1).id)); // top forward
+    if (document.getElementById((original_id + y_lenght) - 1) !== null) surrounding.push(parseInt(document.getElementById((original_id + y_lenght) - 1).id)); // top backward
+    if (document.getElementById(original_id - y_lenght) !== null) surrounding.push(parseInt(document.getElementById(original_id - y_lenght).id)); // bottom
+    if (document.getElementById((original_id - y_lenght) + 1) !== null) surrounding.push(parseInt(document.getElementById((original_id - y_lenght) + 1).id)); // bottom forward
+    if (document.getElementById((original_id - y_lenght) - 1) !== null) surrounding.push(parseInt(document.getElementById((original_id - y_lenght) - 1).id)); // bottom backward
+
+    return surrounding;
 }
