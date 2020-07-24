5cb14418ea4ce6565ad0f34280616d8e48586754
kattogat <maria_hav@live.se>
2020-07-24 16:42:10 +0200

    Beginging on the interface

--- game.js
+++ game.js
@@ -1,12 +1,14 @@
 // 16 x 30, 99 mine
 
 const feild       = document.getElementsByClassName('feild')[0];
+const emoji       = document.getElementsByClassName('emoji')[0];
 const square_size = 1.5; // rem
 let square_amount = 0;
 let mines         = [];
 let x_lenght      = 0;
 let y_lenght      = 0;
 let mine_amount   = 0;
+let timer         = '';
 
 if (!feild.hasChildNodes()) {
     // Create feild if empty
@@ -51,7 +53,7 @@ function createFeild(x, y, mine) {
  * When a player clicks on a square we check that square
  * @param {this} element 
  */
-function check (element) {
+function check(element) {
     if (element.classList.contains('blank-square')) return;
 
     if (feild.querySelectorAll(".blank-square").length < 1) {
@@ -67,6 +69,9 @@ function check (element) {
                 mines[placment] = true;
             }
         }
+
+        // Start timer
+        startTimer();
     }
 
     element.classList.remove("square");
@@ -79,6 +84,13 @@ function check (element) {
         bomb.classList.add("bomb");
         bomb.innerHTML = "💣";
         element.appendChild(bomb);
+
+        // Change emoji
+        emoji.innerHTML = "💀";
+
+        // Stop timer 
+        clearInterval(timer);
+        
         return;
     }
 
@@ -89,6 +101,52 @@ function check (element) {
         element.innerHTML = surrounding_mines;
         return;
     }
+    // else {
+    //     let should_be_checked = closeBy(element.id);
+    //     while (should_be_checked.length > 1) {
+    //         let remove = [];
+    //         for (let i = 0; i < should_be_checked.length; i++) {
+    //             let is_blank = checkSurondingSquares(should_be_checked[i]);
+    //             if (is_blank < 1) {
+    //                 let blank_element = document.getElementById(should_be_checked[i]);
+    //                 blank_element.classList.remove("square");
+    //                 blank_element.classList.add("blank-square");
+    //                 switch (i) {
+    //                     case 0:
+    //                     case 3:
+    //                     case 6:
+    //                         should_be_checked[i] = should_be_checked[i]++;
+    //                         break;
+    //                     case 1:
+    //                     case 4:
+    //                     case 7:
+    //                         should_be_checked[i] = should_be_checked[i]--;
+    //                         break;
+    //                     case 2:
+    //                         should_be_checked[i] = should_be_checked[i] + y_lenght;
+    //                         break;
+    //                     case 5:
+    //                         should_be_checked[i] = should_be_checked[i] - y_lenght;
+    //                         break;
+                    
+    //                     default:
+    //                         break;
+    //                 }
+    //             }
+    //             else {
+    //                 remove.push(i);
+    //             }
+    //         }
+
+    //         if (remove.length > 0) {
+    //             remove.forEach(index => {
+    //                 should_be_checked.splice(index, 1);
+    //             });
+    //         }
+
+    //         console.log(should_be_checked);
+    //     }
+    // }
 }
 
 /**
@@ -124,4 +182,31 @@ function closeBy(original_id) {
     if (document.getElementById((original_id - y_lenght) - 1) !== null) surrounding.push(parseInt(document.getElementById((original_id - y_lenght) - 1).id)); // bottom backward
 
     return surrounding;
+}
+
+function startTimer() {
+    let sec = 0;
+    function pad(val) { 
+        if (val >= 999) {
+            clearInterval(sec);
+            return '999';
+        } 
+        if (val > 99) return val;
+        if (val > 9) return '0' + val;
+        return '00' + val; 
+    }
+    timer = setInterval(function(){
+        document.getElementById("time").innerHTML = pad(++sec);
+    }, 1000);
+}
+
+function resetTimer() {
+    clearInterval(timer);
+    document.getElementById("time").innerHTML = '000';
+}
+
+function reset() {
+    emoji.innerHTML = "🙂";
+    resetTimer();
+    createFeild(x_lenght, y_lenght, mine_amount);
 }
